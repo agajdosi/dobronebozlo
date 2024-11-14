775d09b35b2f3e549dcc46d28a18db600614fcba
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2024-11-14 23:27:36 +0100

    Update the svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -3,27 +3,24 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-            // Optional: specify the output directory (default is 'build')
-            pages: 'build',
-            assets: 'build',
-            fallback: null
-        }),
-		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/dobronebozlo' : ''
-        },
-		prerender: {
-            entries: ['*'] // Prerender all routes
-        }
-	}
+  kit: {
+    adapter: adapter({
+      // Optional: specify the output directory (default is 'build')
+      pages: 'build',
+      assets: 'build',
+      fallback: '404.html', // Use '404.html' for GitHub Pages fallback
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/dobronebozlo' : '',
+      assets: process.env.NODE_ENV === 'production' ? '/dobronebozlo' : '',
+    },
+    prerender: {
+      handleMissingId: 'ignore', // Avoid errors for missing assets
+      entries: ['*'], // Prerender all routes
+    },
+  },
 };
 
 export default config;
