43f5c54536433baf1be211814f3067132d232db5
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2024-11-14 23:48:54 +0100

    update

--- svelte.config.js
+++ svelte.config.js
@@ -13,8 +13,7 @@ const config = {
       fallback: '404.html', // Use '404.html' for GitHub Pages fallback
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/dobronebozlo' : '',
-      assets: process.env.NODE_ENV === 'production' ? 'https://gajdosik.org/dobronebozlo' : '',
+	  base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
     },
     prerender: {
       handleMissingId: 'ignore', // Avoid errors for missing assets
