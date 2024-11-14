fe389a73418277a6b1cf45b0de91a63148aa1dd4
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2024-11-14 23:59:50 +0100

    dasads

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
       fallback: '404.html', // Use '404.html' for GitHub Pages fallback
     }),
     paths: {
-	  base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+	  base: process.env.BASE_PATH || '',
     },
     prerender: {
       handleMissingId: 'ignore', // Avoid errors for missing assets
