e8b557a04c61cf1bfb4cbcabf1a8e153224ba576
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2024-11-14 23:28:40 +0100

    Dadad

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
     }),
     paths: {
       base: process.env.NODE_ENV === 'production' ? '/dobronebozlo' : '',
-      assets: process.env.NODE_ENV === 'production' ? '/dobronebozlo' : '',
+      assets: process.env.NODE_ENV === 'production' ? 'https://gajdosik.org/dobronebozlo' : '',
     },
     prerender: {
       handleMissingId: 'ignore', // Avoid errors for missing assets
