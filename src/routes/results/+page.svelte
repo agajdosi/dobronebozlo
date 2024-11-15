1459f686bbc827e54904b365f7254f4765d2dd18
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2024-11-15 18:47:43 +0100

    Improve the results page

--- src/routes/results/+page.svelte
+++ src/routes/results/+page.svelte
@@ -5,6 +5,8 @@
     import { base } from '$app/paths';
 
     let score = 0;
+    let userName = ''; // New variable to store the username
+    let hasUser = false; // Flag to check if user parameter is present
 
     // Extract the score from the query parameters client-side
     onMount(() => {
@@ -15,6 +17,13 @@
         } catch (error) {
             console.error('Hello looser!');
             score = -1;
+        }
+
+        // Check if username is in URL
+        const nameParam = params.get('user');
+        if (nameParam) {
+            userName = nameParam;
+            hasUser = true;
         }
     });
 
@@ -33,6 +42,13 @@
     function playAgain() {
         goto(`${base}/`);
     }
+
+    function updateURLWithUsername() {
+        // Redirect to the current page with the `user` parameter
+        const params = new URLSearchParams(window.location.search);
+        params.set('user', userName.trim());
+        window.history.replaceState({}, '', `${base}/results?${params.toString()}`);
+    }
 </script>
 
 <svelte:head>
@@ -40,20 +56,60 @@
 </svelte:head>
 
 <main>
-    <h1>{$t('gameResults')}</h1>
-    <p>{$t('yourScore')}: {score}/10</p>
-    <button on:click={playAgain}>{$t('playAgain')}</button>
+    {#if hasUser}
+        <h1>{userName.charAt(0).toUpperCase() + userName.slice(1)} {$t("page.results.thirdPerson")} {score}/10!</h1>
+        <p>{$t("page.results.motivation")}</p>
+        <button class="again" on:click={playAgain}>{$t('page.results.motivationStart')}</button>
+
+    {:else}
+        <h1>{$t('page.results.secondPerson')}: {score}/10</h1>
+        <p>{$t(`page.results.${score}`)}</p>
+
+        <div class="enter">
+            <input
+                type="text"
+                bind:value={userName}
+                placeholder={$t('page.results.enterName')}
+                aria-label="Enter your name"
+            />
+            <button on:click={updateURLWithUsername}>
+                {$t('page.results.saveName')}
+            </button>
+        </div>
+        <button class="again" on:click={playAgain}>{$t('page.results.tryAgain')}</button>
+    {/if}
+
 </main>
 
 <style>
-    main {
-        text-align: center;
-        padding: 20px;
-    }
+main {
+    text-align: center;
+    padding: 20px;
+}
 
-    button {
-        padding: 10px 20px;
-        font-size: 1em;
-        margin-top: 20px;
-    }
+button {
+    padding: 10px 20px;
+    font-size: 1em;
+    margin-top: 20px;
+}
+
+input {
+    padding: 8px;
+    font-size: 1em;
+    margin-right: 10px;
+}
+
+p {
+    font-size: 1.2em;
+    margin-bottom: 20px;
+}
+
+.enter {
+    margin-top: 20vh;
+}
+
+.again {
+    margin-top: 2rem;
+}
+
 </style>
