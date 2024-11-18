92d729940bb6112b794f636799bd34499f1c16cd
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2024-11-18 20:26:03 +0100

    Migrate to adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-
   kit: {
-    adapter: adapter({
-      // Optional: specify the output directory (default is 'build')
-      pages: 'build',
-      assets: 'build',
-      fallback: '404.html', // Use '404.html' for GitHub Pages fallback
-    }),
-    paths: {
-	  base: process.env.BASE_PATH || '',
-    },
-    prerender: {
-      handleMissingId: 'ignore', // Avoid errors for missing assets
-      entries: ['*'], // Prerender all routes
-    },
+    adapter: adapter(),
   },
 };
 
