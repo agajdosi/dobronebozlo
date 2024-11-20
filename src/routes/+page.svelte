0ab88d19f449fca07c90494e86af890fcb42433b
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2024-11-20 18:31:02 +0100

    Add og tags for index file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,22 @@
 
 <svelte:head>
     <title>{$t('page.index.htmlTitle')}</title>
+
+    <!-- Open Graph Meta Tags -->
+    <meta property="og:title" content="Oficiální tréninkový kvíz české diplomacie" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="{base}" />
+    <meta property="og:image" content="{base}/assets/share-image.jpg" />
+    <meta property="og:description" content="Dokážete správně rozlišit dobré a špatné bombardování?" />
+    <meta property="og:site_name" content="dobronebozlo.cz" />
+    <meta property="og:locale" content="cs_CZ" />
+    <meta property="og:locale:alternate" content="en_US" />
+
+    <!-- Twitter Meta Tags (Optional, for better sharing on Twitter) -->
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="Oficiální tréninkový kvíz české diplomacie" />
+    <meta name="twitter:description" content="Dokážete správně rozlišit dobré a špatné bombardování?" />
+    <meta name="twitter:image" content="{base}/assets/share-image.jpg" />
 </svelte:head>
 
 <main>
